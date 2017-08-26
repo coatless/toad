48b2843a4e58061f82343c71c6551c3de35a5caa
James J Balamuta <james.balamuta@gmail.com>
2017-08-26 12:25:19 -0500

    Start working on a macOS preference file

---
+++ macos.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#
+# macos.sh
+# The following file is modified from 
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+
+#### Finder
+
+# Finder: display hidden files/dotfiles by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+
+
+#### 
